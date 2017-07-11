3fe91ea8e0d76d1c2f6dcc6f6c451ef6314b5f39
Hieu <Hieu@hieus-mbp.gateway.sonic.net>
2017-07-11 10:16:14 -0700

    table chart is able to receive json data

---
+++ assets/src/myscript.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+  // display login form
+  $(".login_btn").on("click", function(){
+    $(".login_container").css({"display":"block"});
+  });
+  
+  // exit login form
+  $(".exit_login").on("click", function(){
+    $(".login_container").css({"display":"none"});
+  });
+
+
+  $(".tab_links").on("click", function(e){
+    var tab = "#" + e.currentTarget.innerText.toLowerCase();
+    
+    //remove all button that contains 'active' class
+    $(".tab button.active").removeClass('active');
+    //add 'active' class to btn
+    $(this).addClass('active');
+    
+    //remove 'active' class from children of tab_content 
+    $(".tab_content section.active").removeClass('active');
+    //add 'active' to section with id=tab
+    $(tab).addClass('active');
+  });
+
+  //create table via ally-code.json file
+  $.getJSON("http://127.0.0.1:8080/data/code-test.json", function(json){
+    var rateList = ''
+    var tr = '';
+    var urbank = json.filter(function(val){
+      return val["name"] === "URBank";
+    });
+    var banks = json.filter(function(val){
+      return val["name"] !== "URBank";
+    });
+
+    function createRateChart(obj){
+      var keys = Object.keys(obj);
+      tr += '<tr>';
+      
+      keys.forEach(function(key){
+        switch(key){ 
+          case 'apy':
+            tr += '<td>' + obj[key] + '%</td>';
+            break;
+          case 'earnings':
+            tr += '<td>$' + obj[key] + '</td>';
+            break;
+          default:
+            tr += '<td>' + obj[key] + '</td>'
+        }
+      });
+
+      tr += '</tr>'
+    }
+
+    urbank.forEach(createRateChart);
+    banks.forEach(createRateChart);
+
+    $('tbody').append(tr);
+     
+  })
+
+   
+
+
+
+});
